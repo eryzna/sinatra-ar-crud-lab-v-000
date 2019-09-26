1754b297d226e92cdbd8ebd50e14d47e7be04ffe
Elana Ryznar <eryzna@gmail.com>
2019-09-26 19:27:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,38 +11,44 @@ class ApplicationController < Sinatra::Base
   get '/' do
   end
 
+#create-new-form
   get '/articles/new' do
     erb :new
-
   end
 
+#create-create-save
   post '/articles' do
     Article.create (params)
     #article.save
     redirect to "/articles/#{Article.last.id}"
   end
 
+#read-index
   get '/articles' do
     @articles=Article.all
     erb :index
   end
 
+#read-show
   get "/articles/:id" do
     @article = Article.find(params[:id])
     erb :show
   end
 
+#update-update form
   get '/articles/:id/edit' do
     @article = Article.find(params[:id])
     erb :edit
   end
 
+#update-passes from edit form to params-redirects to updated object
   patch "/articles/:id" do
     @article = Article.find(params[:id])
     @article.update(params[:article])
     redirect to "/articles/#{ @article.id }"
   end
 
+#delete
   delete "/articles/:id" do
     Article.destroy(params[:id])
     redirect to "/articles"
